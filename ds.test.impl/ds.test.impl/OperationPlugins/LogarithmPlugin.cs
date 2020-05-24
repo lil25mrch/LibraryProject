908040d588c54de8973f8cf8309d52138d67ef99
Lilia <lil25mrch@gmail.com>
2020-05-24 22:44:35 +0300

    second Version

--- ds.test.impl/ds.test.impl/OperationPlugins/LogarithmPlugin.cs
+++ ds.test.impl/ds.test.impl/OperationPlugins/LogarithmPlugin.cs
@@ -16,7 +16,11 @@ namespace ds.test.impl.OperationPlugins {
         /// <param name="input1">Логарифм числа</param>
         /// <param name="input2">Основание</param>
         /// <returns>Результат логарифмирования</returns>
+        /// <exception cref="ArgumentException">Ошибка при введении основания меньше 0</exception>
         public override int Run(int input1, int input2) {
+            if (input2 < 0 || input1 < 0 ) {
+                throw new ArgumentException ("Values for logarithm cannot be negative");
+            }
             return checked((int)Math.Log(input1, input2));
         }
     }
--- ds.test.impl/ds.test.impl/Plugins.cs
+++ ds.test.impl/ds.test.impl/Plugins.cs
@@ -5,11 +5,17 @@ using ds.test.impl.OperationPlugins;
 
 namespace ds.test.impl {
     /// <summary>
-    /// Класс с плагинами
+    /// Класс плагинов
     /// </summary>
     public static class Plugins {
+        /// <summary>
+        /// Словарь для хранения названий плагинов и их интерфейсов
+        /// </summary>
         private static readonly Dictionary<string, IPlugin> _pluginsByName = new Dictionary<string, IPlugin>();
 
+        /// <summary>
+        /// Добавление экземпляров плагинов в словарь
+        /// </summary>
         static Plugins() {
             var plugins = new List<IPlugin> {
                 new DivisionPlugin(),
@@ -28,9 +34,22 @@ namespace ds.test.impl {
             }
         }
 
+        /// <summary>
+        /// Количество плагинов
+        /// </summary>
         public static int PluginsCount => _pluginsByName.Count;
+
+        /// <summary>
+        /// Массив названий плагинов
+        /// </summary>
         public static string[] GetPluginNames => _pluginsByName.Keys.ToArray();
 
+        /// <summary>
+        /// Получить плагин из словаря по названию
+        /// </summary>
+        /// <param name="pluginName">Название плагина</param>
+        /// <returns>Плагин</returns>
+        /// <exception cref="ArgumentException">Сообщение об отсутствии плагина с таким названием</exception>
         public static IPlugin GetPlugin(string pluginName) {
             if (!_pluginsByName.TryGetValue(pluginName, out IPlugin result)) {
                 throw new ArgumentException($"Unknown plugin name : {pluginName}");
